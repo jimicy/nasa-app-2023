5ea31db6233593cf1bb4bb55a4a40141ec7367ea
Jimmy Wang <jimmyw22@gmail.com>
2023-10-08 17:29:50 -0700

    Update home page.

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -6,18 +6,26 @@ function Home() {
     <div data-theme="dark" className="hero min-h-screen bg-base-200">
       <div className="hero-content text-center">
         <div className="max-w-xl">
-          <h1 className="text-5xl font-bold">
-            NASA Habitable Exoplanet Adventure Creator
+          <h1 className="text-7xl font-bold">
+            NASA Habitable Exoplanet Adventure
           </h1>
           <p className="py-6">
-            What is an exoplanet? An exoplanet is any planet beyond our solar
-            system. Most orbit other stars, but free-floating exoplanets, called
-            rogue planets, orbit the galactic center and are untethered to any
-            star.
+            <div className="card border">
+              <div className="card-body">
+                <h2 className="card-title">What is an exoplanet?</h2>
+                <p className="text-left">
+                  An exoplanet is any planet beyond our solar system. Most orbit
+                  other stars, but free-floating exoplanets, called rogue
+                  planets, orbit the galactic center and are untethered to any
+                  star.
+                </p>
+              </div>
+            </div>
           </p>
-          <p className="py-4 text-sm">
-            Use publicly available information on habitable worlds to generate a
-            new habitable exoplanet and adventure!
+
+          <p className="py-4 text-sm text-left">
+            Uses publicly available information on habitable worlds to generate
+            a new habitable exoplanet and adventure!
           </p>
           <div className="flex space-x-6 justify-center">
             <a href="/create">
